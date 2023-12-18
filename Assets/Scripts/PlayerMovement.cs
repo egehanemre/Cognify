e12ab8f7c4ce14acd067d054250ec31f4f5296f9
Egehan EMRE <16008122049@ogr.bozok.edu.tr>
2023-12-18 04:14:23 +0300

    Update PlayerMovement.cs

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,7 +6,7 @@ public class PlayerMovement : MonoBehaviour
 {
     private float horizontal;
     private float speed = 8f;
-    private float jumpingPower = 16f;
+    private float jumpingPower = 8f;
     private bool isFacingRight = true;
 
     private bool canDash = true;
